34b69b34e9748badcdd2cc1647ca635bb316d1bf
Ted Yu <tedio85@yahoo.com.tw>
2017-03-07 13:09:55 +0800

    updated Table.java

--- src/stage1/Table.java
+++ src/stage1/Table.java
@@ -4,14 +4,10 @@ import java.util.Set;
 import java.util.Vector;
 
 public interface Table {
-	String name = null;					// name of table
-	Vector<String> params = null;		// sequence of default parameters
-	Set<Vector<Object>> set = null;		// tuples, where the order of elements is as same as params
-	
 	
 	String getName();					// get name of table
-	String getParams();					// get parameters of table
-	String getTable();					// return the object itself
+	Vector<String> getParams();			// get parameters of table
+	Set<Vector<Object>> getTable();		// return the object itself
 	void insert(Vector<Object> tup);	// insert a tuple
 	void show();						// print the table
 }
