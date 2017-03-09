0285da721786d126e432a17a451fb2603bd10355
Ted Yu <tedio85@yahoo.com.tw>
2017-03-09 23:49:38 +0800

    updated Table.java

--- src/stage1/Table.java
+++ src/stage1/Table.java
@@ -1,13 +1,15 @@
 package stage1;
 import java.lang.Object;
-import java.util.Set;
 import java.util.Vector;
 
 public interface Table {
 	
 	String getName();					// get name of table
-	Vector<String> getParams();			// get parameters of table
-	Set<Vector<Object>> getTable();		// return the object itself
+	String getPrimaryKey();				// get primary key
+	Class<?> getPrimaryKeyType();		// get type of primary key
+	Vector<String> getAttrs();			// get names of all attributes
+	Vector<Class<?>> getAttrTypes();	// get parameter type of all attributes of the table
 	void insert(Vector<Object> tup);	// insert a tuple
+	void insertStr(Vector<String> tup);	// insert a tuple with numbers as strings
 	void show();						// print the table
 }
