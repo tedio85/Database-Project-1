8f3fc2380c3e60bcfd62602f6ab54437807b7062
Ted Yu <tedio85@yahoo.com.tw>
2017-03-10 10:25:19 +0800

    modified insert() method to accept strings and tuples with specified attributes

--- src/stage1/Table.java
+++ src/stage1/Table.java
@@ -1,15 +1,12 @@
 package stage1;
-import java.lang.Object;
 import java.util.Vector;
 
 public interface Table {
 	
-	String getName();					// get name of table
-	String getPrimaryKey();				// get primary key
-	Class<?> getPrimaryKeyType();		// get type of primary key
-	Vector<String> getAttrs();			// get names of all attributes
-	Vector<Class<?>> getAttrTypes();	// get parameter type of all attributes of the table
-	void insert(Vector<Object> tup);	// insert a tuple
-	void insertStr(Vector<String> tup);	// insert a tuple with numbers as strings
-	void show();						// print the table
+	String getName();										// get name of table
+	String getPrimaryKey();									// get primary key
+	Vector<Attribute> getAttrs();							// get names of all attributes
+	void insert(Vector<String> tup);						// insert a tuple with numbers as strings
+	void insert(Vector<String> attrNames, Vector<String> tup);	// insert tuple with specified attributes
+	void show();											// print the table
 }
