e92cf2d224e62406a1e08c0b62c87fe8e19faa3a
u10000129 <u10000129@gmail.com>
2017-04-06 09:58:31 +0800

    create TableManager.java and add some naive functions

---
+++ src/stage1/TableManager.java
@@ -0,0 +1,44 @@
+package stage1;
+
+import java.util.HashMap;
+import java.util.Vector;
+
+public class TableManager {
+	private HashMap<String, VectorTable> TableMap;
+	
+	TableManager() {
+		TableMap = new HashMap<String, VectorTable>();
+	}
+	
+	public void putTable(String name, String str_pk, Vector<String> attrs,Vector<String> attrTypes, Vector<Integer> strLen) {
+		TableMap.put(name, new VectorTable(name, str_pk, attrs, attrTypes, strLen));
+	}
+	
+	public void insertTable(String tableName, Vector<String> tup) {
+		TableMap.get(tableName).insert(tup);
+	}
+	
+	public void insertTable(String tableName, Vector<String> attrs, Vector<String> tup) {
+		TableMap.get(tableName).insert(attrs, tup);
+	}
+	
+	public VectorTable getTable(String tableName) {
+		return TableMap.get(tableName);
+	}
+	
+	
+	public int getTableAttrSize(String tableName) {
+		return TableMap.get(tableName).getAttrs().size();
+	}
+	
+	public void dumpCSV() {
+		for (HashMap.Entry<String, VectorTable> entry : TableMap.entrySet())
+    	{
+    	    TableMap.get(entry.getKey()).exportToCSV();
+    	}
+	}
+	
+	public boolean isExist(String tableName) {
+		return TableMap.containsKey(tableName);
+	}
+}
