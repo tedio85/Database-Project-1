d06bb91282c20fc65e66142f9c7bd5b4354d6e78
u10000129 <u10000129@gmail.com>
2017-05-06 20:21:33 +0800

    create QThread2 and implement it with new method

---
+++ src/stage1/CartesianTemp.java
@@ -0,0 +1,11 @@
+package stage1;
+
+public class CartesianTemp {
+	Object p_key1;
+	Object p_key2;
+	
+	CartesianTemp(Object p_key1, Object p_key2) {
+		this.p_key1 = p_key1;
+		this.p_key2 = p_key2;
+	}
+}
