c5da297367e13742855c94ce73c5b0eadf3b9273
u10000129 <u10000129@gmail.com>
2017-05-09 21:55:14 +0800

    renew hashcode and override in CartesianTemp

--- src/stage1/CartesianTemp.java
+++ src/stage1/CartesianTemp.java
@@ -12,18 +12,37 @@ public class CartesianTemp {
 	public void show(){
 		System.out.println("("+p_key1+", "+p_key2+")");
 	}
-	
+
 	@Override
-    public boolean equals(Object that) {
-        if(that instanceof CartesianTemp) {
-            CartesianTemp ct = (CartesianTemp) that;
-            return this.p_key1 == ct.p_key1 && this.p_key2 == ct.p_key2;
-        }
-        return false;
-    }
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((p_key1 == null) ? 0 : p_key1.hashCode());
+		result = prime * result + ((p_key2 == null) ? 0 : p_key2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CartesianTemp other = (CartesianTemp) obj;
+		if (p_key1 == null) {
+			if (other.p_key1 != null)
+				return false;
+		} else if (!p_key1.equals(other.p_key1))
+			return false;
+		if (p_key2 == null) {
+			if (other.p_key2 != null)
+				return false;
+		} else if (!p_key2.equals(other.p_key2))
+			return false;
+		return true;
+	}
 	
-	 @Override
-	    public int hashCode() {
-	        return p_key1.hashCode() + p_key2.hashCode();
-	    }
+	
 }
